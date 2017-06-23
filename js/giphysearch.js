9ec795a5be6ca13e20825739352463d6b2babc2a
Miles Gilbert <miles.gilbert@gmail.com>
2017-06-23 18:41:08 -0500

    added UI, suggested search buttons, open search

--- js/giphysearch.js
+++ js/giphysearch.js
@@ -4,32 +4,59 @@ var preHTML = '<img src="';
 var postHTML = '" />';
 var gifIndex = 0;
 var searchLimit = 500;
+var numResults;
+var onloadSearch = "dance";
 
-var searchUrl = 'https://api.giphy.com/v1/gifs/search?q=dance&api_key=79994bd6eeb14f4d8df417c81a1bd217&limit=' + searchLimit;
+var searchUrlPre = 'https://api.giphy.com/v1/gifs/search?q=';
+var searchUrlPost = '&api_key=79994bd6eeb14f4d8df417c81a1bd217&limit=' + searchLimit;
+
+var bpmSlider = document.getElementById("bpmSlider");
 
 gifContainer = document.getElementById("gif-container");
 
+var salsaButton = document.getElementById("salsaButton");
+var tangoButton = document.getElementById("tangoButton");
+var breakdanceButton = document.getElementById("breakdanceButton");
+var danceSearch = document.getElementById("danceSearch");
+var danceDisplay = document.getElementById("danceDisplay");
+
 window.onload = function() {
-  GetGifs();
-  ShowGif();
+  GetGifs(onloadSearch);
+  // ShowGif();
 };
 
-function GetGifs() {
+function GetGifs(q) {
 	$.ajax({
-	  url: searchUrl,
+	  url: searchUrlPre + q + searchUrlPost,
 	  type: 'GET',
 	  success: function(data) {
 	  	console.log(data);
 		dancegifs = data;
-		//ShowGifs();
+		ShowGif();
 	  }
 	});
 }
 
-//data[4].images.downsized.url
+function keywordSearch() {
+	$.ajax({
+	  url: searchUrlPre + danceSearch.value + searchUrlPost,
+	  type: 'GET',
+	  success: function(data) {
+	  	console.log(data);
+		dancegifs = data;
+		ShowGif();
+		BlurSearch();
+		$(salsaButton).removeClass("active-dance");
+		$(danceButton).removeClass("active-dance");
+		$(breakdanceButton).removeClass("active-dance");
+	  }
+	});
+}
 
 function ShowGif() {
-	var randomNum = Math.floor((Math.random() * (searchLimit-1)) + 1);
+	numResults = Object.keys(dancegifs.data).length;
+
+	var randomNum = Math.floor((Math.random() * (numResults-1)) + 1);
 	setTimeout(function () {
       var dancegif = dancegifs.data[randomNum].images.downsized.url;
 
@@ -39,9 +66,39 @@ function ShowGif() {
 		});
       ShowGif();
 
-      	//gifIndex++;
-      	// if (gifIndex < searchLimit) {
-       //  	ShowGif();
-      	// }
    	}, 1500)
 }
+
+function BlurSearch(){
+	$(danceSearch).blur();
+}
+
+// function DanceDisplay(danceType){
+// 	$(danceDisplay).text(danceType);
+//   	$(danceDisplay).toggleClass('animated zoomInDown');
+//   	$(danceDisplay).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+//   	$(danceDisplay).text("");
+//   });
+// }
+
+$(danceSearch).focus(function() {
+	$(this).val("");
+});
+
+$(salsaButton).click(function() {
+  	GetGifs("salsa dancing");
+  	$(this).toggleClass("active-dance");
+  	$(this).siblings().removeClass("active-dance");
+});
+
+$(danceButton).click(function() {
+  	GetGifs("dance");
+  	$(this).toggleClass("active-dance");
+  	$(this).siblings().removeClass("active-dance");
+});
+
+$(breakdanceButton).click(function() {
+  	GetGifs("breakdancing");
+  	$(this).toggleClass("active-dance");
+  	$(this).siblings().removeClass("active-dance");
+});
