dcb462aa020e7cfedc1584560ff5e3455b45a2ef
Miles Gilbert <miles.gilbert@gmail.com>
2017-07-09 01:21:17 -0500

    new ui, faster loading gifs, iphone friendly

--- js/gifclub.js
+++ js/gifclub.js
@@ -1,69 +1,37 @@
-// GIFCLUB TM CHANNELS
-
-var channel1 = {
-    "name": "Channel 1",
-    "playlist": ["https://soundcloud.com/miles-gilbert-2/sets/visuals-thegif-club"],
-    "visuals":  ["dance", "soccer dance", "mom dance", "oprah"]
-}
-var channel2 = {
-    "name": "HYPE THO",
-    "songs": ["https://soundcloud.com/essential_music/baewolf-lost-boys-edm-com", "https://soundcloud.com/flosstradamus/flosstradamus-troyboi-soundclash", "https://soundcloud.com/splitbreed/splitbreed-walkers-free-dl-for-the-first-2000", "https://soundcloud.com/leaf-jones/meek-mill-im-a-boss-feat-rick"],
-    "visuals":  ["explosions", "squad goals", "zombies", "like a boss"]
-}
-var channel3 = {
-    "name": "Cute 💚",
-    "playlist": ["https://soundcloud.com/miles-gilbert-2/sets/cute-visuals-thegif-club"],
-    "visuals":  ["love cute", "cute", "rainbows", "space"]
-}
-
 // VARS
 
-var activeVisualsArray;
-var activeChannel;
 var litModeSwitch = document.getElementById("modeSwitch");
 var litMode = true;
-var channel1Button = document.getElementById("channel1Button");
-var channel2Button = document.getElementById("channel2Button");
-var channel3Button = document.getElementById("channel3Button");
-var customChannelButton = document.getElementById("customChannelButton");
+var litModeContainer = document.getElementById("litModeContainer");
 var limitButton5 = document.getElementById("limitButton5");
 var limitButton50 = document.getElementById("limitButton50");
 var limitButton500 = document.getElementById("limitButton500");
 var videoBackground = document.getElementById("gif-container");
 var popupGridWrapper = document.getElementById("popupGridWrapper");
-var litModeContainer = document.getElementById("litModeContainer");
-var soundcloudContainer = document.getElementById("soundcloudContainer");
-var emptyPopupGrid = '<div id="popupGif-1" class="popup-gif"></div><div id="popupGif-2" class="popup-gif"></div><div id="popupGif-3" class="popup-gif"></div><div id="popupGif-4" class="popup-gif"></div><div id="popupGif-5" class="popup-gif"></div><div id="popupGif-6" class="popup-gif"></div><div id="popupGif-7" class="popup-gif"></div><div id="popupGif-8" class="popup-gif"></div><div id="popupGif-9" class="popup-gif"></div><div id="popupGif-10" class="popup-gif"></div><div id="popupGif-14" class="popup-gif"></div><div id="popupGif-15" class="popup-gif"></div><div id="popupGif-16" class="popup-gif"></div>';
-var addTrackButton = document.getElementById("addTrackButton");
-var customPlaylistTable = document.getElementById("customPlaylistTable");
-var clearCustomPlaylistButton = document.getElementById("clearCustomPlaylistButton");
-var startCustomPlaylistButton = document.getElementById("startCustomPlaylistButton");
-var customTrackCount;
-var newTrackHTML = '<tr><td id="songInput"><input id="customTrackUrl" placeholder="SoundCloud URL" type="text"></td><td id="visualInput"><input id="customGiphySearch" placeholder="GIPHY Search" type="text"></td><td class="track-delete-btn"><i class="material-icons">delete</i></td></tr>';
-var customChannel = {};
-var customChannelActive = false;
-var isPlaylist;
-
-var fullscreenSearchButton = document.getElementById("fullscreenSearchButton");
-var fullscreenSearch = document.getElementById("fullscreenSearch");
-
+var emptyPopupGrid = '<div id="popupGif-1" class="popup-gif magictime"></div><div id="popupGif-2" class="popup-gif magictime"></div><div id="popupGif-3" class="popup-gif magictime"></div><div id="popupGif-4" class="popup-gif magictime"></div><div id="popupGif-5" class="popup-gif magictime"></div><div id="popupGif-6" class="popup-gif magictime"></div><div id="popupGif-7" class="popup-gif magictime"></div><div id="popupGif-8" class="popup-gif magictime"></div><div id="popupGif-9" class="popup-gif magictime"></div><div id="popupGif-10" class="popup-gif magictime"></div><div id="popupGif-14" class="popup-gif magictime"></div><div id="popupGif-15" class="popup-gif magictime"></div><div id="popupGif-16" class="popup-gif magictime"></div>';
 var giphySearch = document.getElementById("giphySearch");
+var uiContainer = document.getElementById("uiContainer");
+var moodsContainer = document.getElementById("moodsContainer");
+var searchButton = document.getElementById("searchButton");
+var animations = ['slideUpReturn', 'slideDownReturn','slideRightReturn','slideLeftReturn', 'puffIn'];
 
 
 // PLATFORM CHECK
 
-if (getOS() != "iOS") {
-    window.onload = function() {
-        GetGifs("dance");
-        $(giphySearch).focus();
-    };
-} else {
-    videoBackground.innerHTML = '<h4 class="white-text" style="padding: 80px;">Sorry, iOS does not allow for autoplay of videos and gifclub is all about that. Please visit us on desktop or Android.';
-    soundcloudContainer.innerHTML = '';
-}
+// if (getOS() != "iOS") {
+//     window.onload = function() {
+//         GetGifs("dance");
+//         $(giphySearch).focus();
+//     };
+// } else {
+//     videoBackground.innerHTML = '<h4 class="white-text" style="padding: 80px;">Sorry, iOS does not allow for autoplay of videos and gifclub is all about that. Please visit us on desktop or Android.';
+//     soundcloudContainer.innerHTML = '';
+// }
 
-// CHANNEL FUNCTIONS
-
+window.onload = function() {
+    GetTrending();
+    $(giphySearch).focus();
+};
 
 // SEARCH FUNCTIONS
 
@@ -89,33 +57,24 @@ function BlurSearch(){
     $(giphySearch).blur();
 }
 
-function ToggleSearch() {
-    $(giphySearch).toggle();
+function ToggleUI() {
+    $(uiContainer).fadeToggle(300);
     $(giphySearch).focus();
+    $(giphySearch).select();
 }
 
-// MISC FUNCTIONS
-
-$('.modal').modal({
-    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
-    inDuration: 300, // Transition in duration
-    outDuration: 200, // Transition out duration
-    startingTop: '4%', // Starting top style attribute
-    endingTop: '10%', // Ending top style attribute
-    // ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-    //   alert("Ready");
-    //   console.log(modal, trigger);
-    // },
-    // complete: function() { alert('Closed'); } // Callback for Modal close
-  }
-);
-
-$(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
+$('.mood-channel').click(function() {
+    var q = $(this).data("query");
+    MoodSearch(q);
 });
 
-$(popupGridWrapper).click(function(){
-    ToggleSearch();
-})
+$(popupGridWrapper).click(function() {
+    ToggleUI();
+});
+
+$(document).ready(function(){
+    $(moodsContainer).children().each(function(i){
+        var color = randomColor({ luminosity: 'bright'});
+        $(this).css("color", color);
+    });
+});
--- js/giphysearch.js
+++ js/giphysearch.js
@@ -2,15 +2,28 @@ var channelgifs;
 var preHTML = '<img src="';
 var postHTML = '" />';
 var gifIndex = 0;
-var searchLimit = 50;
+var searchLimit = 100;
 var numResults;
 
 var searchUrlPre = 'https://api.giphy.com/v1/gifs/search?q=';
+var trendingUrl = 'https://api.giphy.com/v1/gifs/trending?api_key=' + config + '&limit=50';
 var searchUrlPost = '&api_key=' + config + '&limit=';
 
 function GetGifs(q) {
 	$.ajax({
 	  url: searchUrlPre + q + searchUrlPost + searchLimit,
+	  type: 'GET',
+	  success: function(data) {
+		channelgifs = data;
+		console.log(channelgifs);
+		ShowGif();
+	  }
+	});
+}
+
+function GetTrending() {
+	$.ajax({
+	  url: trendingUrl,
 	  type: 'GET',
 	  success: function(data) {
 		channelgifs = data;
@@ -21,17 +34,41 @@ function GetGifs(q) {
 
 $(giphySearch).keydown(function( event ) {
 	if ( event.which == 13 ) {
-		$.ajax({
+		CustomSearch();
+	}
+});
+
+$(searchButton).click(function(){
+	if (giphySearch.value != '') {
+		CustomSearch();
+	} else {
+		giphySearch.value = "Enter something";
+	}
+})
+
+function CustomSearch() {
+	$.ajax({
 	  	url: searchUrlPre + giphySearch.value + searchUrlPost + searchLimit,
 	  	type: 'GET',
 	  	success: function(data) {
 		channelgifs = data;
 		ShowGif();
-		ToggleSearch();
+		ToggleUI();
 	  }
 	});
-	}
-});
+}
+
+function MoodSearch(q) {
+	$.ajax({
+	  	url: searchUrlPre + q + searchUrlPost + searchLimit,
+	  	type: 'GET',
+	  	success: function(data) {
+		channelgifs = data;
+		ShowGif();
+		ToggleUI();
+	  }
+	});
+}
 
 function ShowGif() {
 	numResults = Object.keys(channelgifs.data).length;
@@ -40,33 +77,30 @@ function ShowGif() {
 	
 
 	setTimeout(function () {
-		if(channelgifs.data[randomNum].images.original_mp4.mp4 != undefined) {
+		if(channelgifs.data[randomNum].images.original_mp4.mp4) {
       		var channelgif = channelgifs.data[randomNum].images.original_mp4.mp4;
      	}
       	
       	videoBackground.innerHTML = '<video autoplay loop playsinline id="video-background" muted><source src="' + channelgif + '"></video>';
 
-      	//litMode = $(litModeSwitch).is(':checked');
+  		$(litModeContainer).addClass('lit-mode-bg');
 
-      	if (litMode) {
-      		$(litModeContainer).addClass('lit-mode-bg');
-      		var randomNum2 = Math.floor((Math.random() * (numResults-1)));
-      		var randomCell = Math.floor((Math.random() * 16) + 1);
-      		var randomPopup = document.getElementById('popupGif-' + randomCell);
-      		var randomDepth = Math.floor((Math.random() * 5) + 1);
-      		if (channelgifs.data[randomNum2].images.original_mp4.mp4 != undefined) {
-      			var channelgifPopup = channelgifs.data[randomNum2].images.original_mp4.mp4;
-      		}
+  		var randomNum2 = Math.floor((Math.random() * (numResults-1)));
+  		var randomCell = Math.floor((Math.random() * 16) + 1);
+  		var randomPopup = document.getElementById('popupGif-' + randomCell);
+  		var randomDepth = Math.floor((Math.random() * 5) + 1);
+  		if (Math.floor(Math.random() * 2) == 1) {
+  			var randomAnimation = animations[Math.floor((Math.random() * animations.length) + 1)];
+  			$(randomPopup).addClass(randomAnimation);
+  		}
+  		if (channelgifs.data[randomNum2].images.preview.mp4 != undefined) {
+  			var channelgifPopup = channelgifs.data[randomNum2].images.preview.mp4;
+  		}
 
-      		var i = 0;
+  		var i = 0;
 
-	      	if (randomPopup){
-	      		randomPopup.innerHTML = '<video autoplay loop playsinline id="video-background" class="br-2 z-depth-' + randomDepth +'" muted><source src="' + channelgifPopup + '"></video>';
-	      	}
-
-      	} else if (popupGridWrapper.innerHTML != emptyPopupGrid) {
-      		$(litModeContainer).removeClass('lit-mode-bg');
-      		popupGridWrapper.innerHTML = emptyPopupGrid;
+      	if (randomPopup){
+      		randomPopup.innerHTML = '<video autoplay loop playsinline id="video-background" class="br-2 z-depth-' + randomDepth +'" muted><source src="' + channelgifPopup + '"></video>';
       	}
       	
       	ShowGif();
