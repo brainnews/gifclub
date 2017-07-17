420a38578df20868fc1163ba90a5c88b45e86a1f
Miles Gilbert <miles.gilbert@gmail.com>
2017-07-17 07:57:34 -0500

    building av engine

--- js/selects.js
+++ js/selects.js
@@ -5,22 +5,24 @@ var selects = {
 			"00:00:00": "space trippy",
         	"00:00:49": "rocket launch",
         	"00:01:00": "city timelapse",
-        	"00:01:18": "jay z",
+        	"00:01:18": "nyc",
         	"00:02:00": "city timelapse",
         	"00:03:00": "sex and the city",
         	"00:03:30": "city timelapse",
         	"00:04:06": "rnb flirt"
 		}
 	},
-	"kobe": {
-		"playlist": ["https://soundcloud.com/akajoe/lil-wayne-kobe-bryant"],
+	"cute": {
+		"playlist": ["https://soundcloud.com/miles-gilbert-2/sets/cute-visuals-thegif-club"],
+		"duration": 634000,
 		"timeline": {
-			"00:00:00": "come at me bro",
-			"00:00:14": "running",
-        	"00:00:22": "lakers",
-        	"00:00:44": "kobe bryant",
-        	"00:00:48": "lil wayne",
-        	"00:01:00": "kobe lil wayne",
+			"00:00:00": "love cute",
+        	"00:00:24": "kiss",
+        	"00:00:50": "flirt",
+        	"00:01:17": "hug cute",
+        	"00:01:44": "love",
+        	"00:02:10": "cute",
+        	"00:02:22": "cute kiss",
 		}
 	}
 }
